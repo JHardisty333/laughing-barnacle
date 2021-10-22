b9264be230b89d899408d5c90fd24383ab155603
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-21 20:07:12 -0600

    tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,64 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  })
+    .then(products => {
+      res.json(products)
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+    .then(product => {
+      res.json(product)
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(createTag => {
+      res.json(createTag)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(updateTag => {
+      res.json(updateTag)
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(deleted => {
+      res.json({ ...deleted, msg: 'Tag successfully removed.' })
+    })
 });
 
 module.exports = router;
