44bea0cf7d94426a303fe677cb9b2e9eb10ff08e
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-21 19:58:56 -0600

    product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    }]
+  })
+  .then(products => {
+    res.json(products)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    }]
+  })
+  .then(products => {
+    res.json(products)
+  })
 });
 
 // create new product
@@ -91,6 +112,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(deleted => {
+    res.json({...deleted, msg: 'Product successfully removed.'})
+  })
 });
 
 module.exports = router;
